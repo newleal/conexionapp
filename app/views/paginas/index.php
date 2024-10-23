99c05f2175b953d2a4304ef6eabe0a33df6c1fd3
newleal <lealb.miguelangel@gmail.com>
2024-10-22 19:37:35 -0500

    22-10-2024 reinicio de datos de aplicacion

--- app/views/paginas/index.php
+++
@@ -1,8 +0,0 @@
-<?php require_once(APPROOT . '/views/shared/header.php'); ?>
-<div class="jumbotron">
-    <h1 class="dispplay-3"><?php echo $data['titulo']; ?></h1>
-
-    <p>Esta es una aplicacion desarrollada por Miguel Angel</p>
-    Version: <span class="badge badge-info"><?php echo APPVERSION; ?></span>
-</div>
-<?php require_once(APPROOT . '/views/shared/footer.php'); ?>
--- public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-//cargar datos desde archivo de inicio app
-
-require_once('../app/bootstrap.php');
-
-
-//inicializar el core
-$init = new Core();
