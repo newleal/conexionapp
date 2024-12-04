3f7ab5cb9235d1484172056fc347f396145532b5
newleal <lealb.miguelangel@gmail.com>
2024-12-04 16:35:20 -0500

    4-12-2024 Reinicio de la aplicaicon

--- app/views/paginas/about.php
+++
@@ -1,7 +0,0 @@
-<?php include_once (APPROOT . '/views/shared/header.php') ?>
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hola desde About</h1>
-    Version:  <span class="btn btn-info"><?php echo VERSION; ?></span>
-<?php include_once (APPROOT . '/views/shared/footer.php') ?>
--- public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * puerta de la aplicacion index de la aplicaicon
- */
-
- require_once ('../app/bootsatrap.php');
-
- //instanciamos la clase core
- $init = new Core;
